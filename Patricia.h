bfb3edefa959e98b7fb5a0d2932045402121c963
primary157 <victorgvbh@gmail.com>
2017-04-04 20:18:12 -0300

    Patricia functions added

--- Patricia.h
+++ Patricia.h
@@ -12,5 +12,10 @@ typedef struct _ExtNode{
 	Chave c;
 } ExtNode;
 union Node{ IntNode i; ExtNode e;};
+void insertPatricia(Node**,Chave*);
+void searchPatricia(Node*,Chave);
+void disablePatricia(Node**,Chave);
+void printPatricia(Node*);
+void initPatricia(Node**);
 
 #endif
