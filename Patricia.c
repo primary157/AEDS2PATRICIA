3871c7a4a82fa34d6cf974d328ea2b65f6d23e44
primary157 <victorgvbh@gmail.com>
2017-04-07 16:03:37 -0300

    Pesquisa concluida

--- Patricia.c
+++ Patricia.c
@@ -12,5 +12,9 @@ Boolean IsExternal(Node n){
 void searchPatricia(Node* n, Data d){
 	if(IsExternal(*n) == True){
 		if(d == n->NodeInfo.e) printf("Elemento Encontrado\n");
+		else printf("Ops nao achei\n");
+		return;
 	}
+	if(bit(n->NodeInfo.i.position, d) == False) searchPatricia(n->NodeInfo.i.lnode,d);
+	else searchPatricia(n->NodeInfo.i.rnode,d);
 }
