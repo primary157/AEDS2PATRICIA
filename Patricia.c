19a6b68f430b90886aac7b2612e921f9afd5fc3e
primary157 <victorgvbh@gmail.com>
2017-04-07 21:15:15 -0300

    correcao bit

--- Patricia.c
+++ Patricia.c
@@ -3,7 +3,8 @@ Boolean bit(unsigned int position, Data x){
 	unsigned int i;
 	if (position == 0) return False;
 	else{ 
-		for(i = 1; i <= x - position; i++) x = x>>1; return (Boolean)(x&1);
+		for(i = 1; i <= 8 - position; i++) x = x>>1; 
+		return (Boolean)(x&1);
 	}
 }
 Boolean IsExternal(Node n){
